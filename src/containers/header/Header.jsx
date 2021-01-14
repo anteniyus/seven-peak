9b6c784590c2cde40f2cf967795481682858a136
Reza <development.rezasajadi@gmail.com>
2021-01-14 13:16:04 +0330

    Change Bookmark to link

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,46 +1,19 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import styles from "./Header.module.css";
 import AppContext from "../../AppContext";
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      enableRedirect: false,
-    };
-  }
-
-  componentDidMount() {
-    console.log("mount");
-  }
-
-  showBookmarks = () => {
-    console.log(this.context);
-    this.setState({ enableRedirect: true });
-  };
-
   render() {
-    const { enableRedirect } = this.state;
     return (
       <>
         <div className={styles.header}>
           <div className={styles.headerRight}>
-            <button type="button" onClick={this.showBookmarks}>
-              All Bookmarks
-            </button>
+            <Link to="/allBookmarks">All Bookmarks</Link>
           </div>
         </div>
-        {enableRedirect && (
-          <Redirect
-            to={{
-              pathname: "/allBookmarks",
-            }}
-            push
-          />
-        )}
       </>
     );
   }

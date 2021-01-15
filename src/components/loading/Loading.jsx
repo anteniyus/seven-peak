95bd5aca1a79bc0d6d74d04bd49bda087ed96a00
Reza <development.rezasajadi@gmail.com>
2021-01-16 00:41:10 +0330

    Added new loading.

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,22 +1,8 @@
 import React from "react";
-import { AiOutlineLoading3Quarters } from "react-icons/ai";
-import { IconContext } from "react-icons";
-import styled from "styled-components";
-
-const CenteredDiv = styled.div`
-  text-align: center;
-`;
+import styles from "./Loading.module.css";
 
 function Loading() {
-  return (
-    <CenteredDiv>
-      <IconContext.Provider value={{ color: "rgb(12, 53, 115)", size: "3em" }}>
-        <div>
-          <AiOutlineLoading3Quarters style={{ margin: "15%" }} />
-        </div>
-      </IconContext.Provider>
-    </CenteredDiv>
-  );
+  return <div className={styles.loader} />;
 }
 
 export default Loading;
